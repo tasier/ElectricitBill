93afdd9e036d619b6757d18f9266c664fa357670
燕乙 <binjie.sbj@cainiao.com>
2018-11-03 15:03:36 +0800

    2018-10计费

--- sh/batch.sh
+++ sh/batch.sh
@@ -6,7 +6,7 @@ tmp_file="/tmp/params"
 echo ${tmp_file} | xargs ./calc_batch_params.sh  
 
 #计算结果一行一个的命令
-# echo ${tmp_file} | xargs  ./extract_electric.sh 
+# echo ${tmp_file} | xargs  ./extract_electric.sh
 
 #如果需要将计算结果放到一行，可以使用下面的命令
 echo ${tmp_file} | xargs  ./extract_electric.sh | xargs | sed 's/ /, /g'
